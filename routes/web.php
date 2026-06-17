ae097d3dbd783cdb5c6f19b73f0cdb647a6217a7
Jose Rafael <jose.rafael1611@gmail.com>
2026-06-17 15:50:31 +0700

    feat: add update and destroy routes for settlements, expenses, chores, shopping

--- routes/web.php
+++ routes/web.php
@@ -17,14 +17,22 @@ Route::delete('/members/{member}', [MemberController::class, 'destroy'])->name('
 
 Route::get('/expenses', [ExpenseController::class, 'index'])->name('expenses.index');
 Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
+Route::put('/expenses/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
+Route::delete('/expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
 
 Route::get('/settlements', [SettlementController::class, 'index'])->name('settlements.index');
 Route::post('/settlements', [SettlementController::class, 'store'])->name('settlements.store');
+Route::put('/settlements/{settlement}', [SettlementController::class, 'update'])->name('settlements.update');
+Route::delete('/settlements/{settlement}', [SettlementController::class, 'destroy'])->name('settlements.destroy');
 
 Route::get('/chores', [ChoreController::class, 'index'])->name('chores.index');
 Route::post('/chores', [ChoreController::class, 'store'])->name('chores.store');
+Route::put('/chores/{chore}', [ChoreController::class, 'update'])->name('chores.update');
+Route::delete('/chores/{chore}', [ChoreController::class, 'destroy'])->name('chores.destroy');
 Route::patch('/chores/assignments/{assignment}/complete', [ChoreController::class, 'complete'])->name('chores.assignments.complete');
 
 Route::get('/shopping', [ShoppingController::class, 'index'])->name('shopping.index');
 Route::post('/shopping', [ShoppingController::class, 'store'])->name('shopping.store');
+Route::put('/shopping/{shoppingItem}', [ShoppingController::class, 'update'])->name('shopping.update');
+Route::delete('/shopping/{shoppingItem}', [ShoppingController::class, 'destroy'])->name('shopping.destroy');
 Route::post('/shopping/{shoppingItem}/purchase', [ShoppingController::class, 'markPurchased'])->name('shopping.purchase.store');
