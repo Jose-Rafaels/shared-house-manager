5c3fa9af30aa1250cf8e295ef52308928b17b174
Jose Rafael <jose.rafael1611@gmail.com>
2026-08-03 11:13:06 +0700

    remove is_fully_settled accessor from Expense

--- app/Models/Expense.php
+++ app/Models/Expense.php
@@ -35,11 +35,4 @@ class Expense extends Model
     {
         return $this->hasMany(ExpenseSplit::class);
     }
-
-    public function getIsFullySettledAttribute(): bool
-    {
-        $splits = $this->splits;
-
-        return $splits->isNotEmpty() && $splits->every->is_settled;
-    }
 }
