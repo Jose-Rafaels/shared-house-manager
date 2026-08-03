2fad9d4ccededc0650eee349fa624da9bf99d2f2
Jose Rafael <jose.rafael1611@gmail.com>
2026-08-03 11:16:02 +0700

    remove expense update and split toggle routes

--- routes/web.php
+++ routes/web.php
@@ -17,9 +17,7 @@ Route::delete('/members/{member}', [MemberController::class, 'destroy'])->name('
 
 Route::get('/expenses', [ExpenseController::class, 'index'])->name('expenses.index');
 Route::post('/expenses', [ExpenseController::class, 'store'])->name('expenses.store');
-Route::put('/expenses/{expense}', [ExpenseController::class, 'update'])->name('expenses.update');
 Route::delete('/expenses/{expense}', [ExpenseController::class, 'destroy'])->name('expenses.destroy');
-Route::patch('/expense-splits/{split}/settled', [ExpenseController::class, 'toggleSplit'])->name('expense-splits.toggle');
 
 Route::get('/settlements', [SettlementController::class, 'index'])->name('settlements.index');
 Route::post('/settlements', [SettlementController::class, 'store'])->name('settlements.store');
